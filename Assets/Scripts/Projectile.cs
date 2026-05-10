ad3ab4137b15b8ed1ce6b0d26b9e8884b581ef7d
DURKESH-KUMAR <durkesh2004kumar@gmail.com>
2026-05-10 17:19:57 +0530

    fireball

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,6 +5,7 @@ public class Projectile : MonoBehaviour
     [SerializeField] private float speed;
     private float direction;
     private bool hit;
+    private float lifetime;
     private Animator anim;
     private BoxCollider2D boxCollider;
     private void Awake()
@@ -20,15 +21,18 @@ public class Projectile : MonoBehaviour
         }
         float movementSpeed=speed*Time.deltaTime*direction;
         transform.Translate(movementSpeed,0,0);
+        lifetime+=Time.deltaTime;
+        if(lifetime>5)gameObject.SetActive(false);
     }
-    private void OnTriggerEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        hit=true;
-        boxCollider.enabled=false;
+        hit = true;
+        boxCollider.enabled = false;
         anim.SetTrigger("explode");
     }
     public void SetDirection(float _direction)
     {
+        lifetime=0;
         direction=_direction;
         gameObject.SetActive(true);
         hit=false;
