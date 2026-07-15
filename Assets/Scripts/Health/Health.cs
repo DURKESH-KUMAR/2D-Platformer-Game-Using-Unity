dd27bbffca2f4c168735c35d4e7148406f530e5c
DURKESH-KUMAR <durkesh2004kumar@gmail.com>
2026-07-15 21:46:42 +0530

    Health Collectables

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -34,4 +34,8 @@ public class Health:MonoBehaviour
             TakeDamage(1);
         }
     }
+    public void AddHealth(float _value)
+    {
+        currentHealth =Mathf.Clamp(currentHealth +_value,0,startingHealth);
+    }
 }
