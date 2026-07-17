54a507e9866f4484ece86bfb6c9f72471ee83a58
DURKESH-KUMAR <durkesh2004kumar@gmail.com>
2026-07-17 23:00:31 +0530

    Enemy Patrol

--- Assets/Scripts/Enemy/MeleeEnemy.cs
+++ Assets/Scripts/Enemy/MeleeEnemy.cs
@@ -1,18 +1,25 @@
 using UnityEngine;
 public class MeleeEnemy : MonoBehaviour
 {
+    [Header("Attack Parameters")]
     [SerializeField] private float attackCoolDown;
     [SerializeField] private float range;
-    [SerializeField] private float colliderDistance;
     [SerializeField] private int damage;
+    [Header("Collider Parameters")]
+    [SerializeField] private float colliderDistance;
+    
     [SerializeField] private BoxCollider2D boxCollider;
+    [Header("Player Layer")]
     [SerializeField] private LayerMask playerLayer;
     private float cooldownTimer=Mathf.Infinity;
     private Animator anim;
     private Health playerHealth;
+    private EnemyPatrol enemyPatrol;
     private void Awake()
     {
         anim=GetComponent<Animator>();
+        enemyPatrol=GetComponentInParent<EnemyPatrol>();
+
     }
     private void Update()
     {
@@ -25,6 +32,10 @@ public class MeleeEnemy : MonoBehaviour
                 anim.SetTrigger("meleeAttack");
                 
             } 
+        }
+        if (enemyPatrol != null)
+        {
+            enemyPatrol.enabled=!PlayerInSight();
         }
         
     }
