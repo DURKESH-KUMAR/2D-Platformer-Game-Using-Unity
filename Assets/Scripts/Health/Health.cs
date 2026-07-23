7d594c743a9feecef40fce64b24d882d1ee4fabb
DURKESH-KUMAR <durkesh2004kumar@gmail.com>
2026-07-23 17:02:25 +0530

    Respawn & Checkpoint

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -71,6 +71,18 @@ public class Health:MonoBehaviour
     {
         currentHealth =Mathf.Clamp(currentHealth +_value,0,startingHealth);
     }
+    public void Respawn()
+    {
+        dead=false;
+        AddHealth(startingHealth);
+        anim.ResetTrigger("die");
+        anim.Play("Idle");
+        StartCoroutine(Invunerability());
+        foreach(Behaviour component in components)
+        {
+            component.enabled=true;
+        }
+    }
     private IEnumerator Invunerability()
     {
         Physics2D.IgnoreLayerCollision(7,8,true);
