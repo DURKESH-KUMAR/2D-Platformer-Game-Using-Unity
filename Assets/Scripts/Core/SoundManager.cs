73f7795fecf62a6d6bab75c6233c925c2d932dbf
DURKESH-KUMAR <durkesh2004kumar@gmail.com>
2026-07-24 10:33:52 +0530

    Music & Volume

--- Assets/Scripts/Core/SoundManager.cs
+++ Assets/Scripts/Core/SoundManager.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
     public static SoundManager instance{get;private set;}
-    private AudioSource source;
+    private AudioSource soundSource;
+    private AudioSource musicSource;
     private void Awake()
     {
-        source =GetComponent<AudioSource>();
+        soundSource=GetComponent<AudioSource>();
+        musicSource=transform.GetChild(0).GetComponent<AudioSource>();
         if (instance == null)
         {
             instance=this;
@@ -14,9 +16,34 @@ public class SoundManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+        ChangeMusicVolume(0);
+        ChangeSoundVolume(0);
     }
     public void PlaySound(AudioClip _sound)
     {
-        source.PlayOneShot(_sound);
+        soundSource.PlayOneShot(_sound);
+    }
+    public void ChangeSoundVolume(float _change)
+    {
+        ChangeSourceVolume(1,"soundVolume",_change,soundSource);
+    }
+    public void ChangeMusicVolume(float _change)
+    {
+        ChangeSourceVolume(0.3f,"musicVolume",_change,musicSource);
+    }
+    private void ChangeSourceVolume(float baseVolume,string volumeName,float change,AudioSource source)
+    {
+        float currentVolume=PlayerPrefs.GetFloat(volumeName,1);
+        currentVolume+=change;
+        if (currentVolume > 1)
+        {
+            currentVolume=0;
+        }else if (currentVolume < 0)
+        {
+            currentVolume=1;
+        }
+        float finalVolume=currentVolume*baseVolume;
+        source.volume=finalVolume;
+        PlayerPrefs.SetFloat("soundVolume",currentVolume);
     }
 }
